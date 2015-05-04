1fab2710a3943fd5732574865379aca6e380ee38
Morten Frydensberg <mortenfryd@gmail.com>
2015-05-04 20:06:01 +0200

    calender working

--- skoleeventkalender/eventview.aspx
+++ skoleeventkalender/eventview.aspx
@@ -1,5 +1,8 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="eventview.aspx.cs" Inherits="skoleeventkalender.WebForm1" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="eventview.aspx.cs" Inherits="skoleeventkalender.eventView" %>
+
+<%@ Register Assembly="DayPilot" Namespace="DayPilot.Web.Ui" TagPrefix="DayPilot" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <DayPilot:DayPilotCalendar ID="eventCalender" runat="server" />
 </asp:Content>
--- skoleeventkalender/eventview.aspx.cs
+++ skoleeventkalender/eventview.aspx.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace skoleeventkalender
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class eventView : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            eventCalender.DataSource = getTestData();
+
+            eventCalender.DataStartField = "start";
+            eventCalender.DataEndField = "end";
+            eventCalender.DataTextField = "name";
+            eventCalender.DataIdField = "id";
+
+            eventCalender.StartDate = DateTime.Now;
+            eventCalender.Days = 5;
+
+            if (!IsPostBack)
+                DataBind();
+
+        }
+
+        private DataTable getTestData()
+        {
+
+            DataTable dt;
+            dt = new DataTable();
+            dt.Columns.Add("start", typeof(DateTime));
+            dt.Columns.Add("end", typeof(DateTime));
+            dt.Columns.Add("name", typeof(string));
+            dt.Columns.Add("id", typeof(string));
+
+            DataRow dr;
+
+            dr = dt.NewRow();
+            dr["id"] = 0;
+            dr["start"] = Convert.ToDateTime("15:30").AddDays(1);
+            Response.Write(dr["start"].ToString());
+            dr["end"] = Convert.ToDateTime("16:30").AddDays(1);
+            dr["name"] = "Partner conf. call";
+            dt.Rows.Add(dr);
+
+            // ...
+
+            return dt;
 
         }
     }
